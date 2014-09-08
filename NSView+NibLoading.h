33490a3a20f46c966bef8a1373407a07a148b623
Peter Paulis <peter.paulis@gmail.com>
2014-09-08 12:13:27 +0200

    Update NSView+NibLoading.h

--- NSView+NibLoading.h
+++ NSView+NibLoading.h
@@ -10,6 +10,8 @@
 
 @interface NSView (NibLoading)
 
-+ (NSView *)loadWithNibNamed:(NSString *)nibNamed owner:(id)owner class:(Class)loadClass;
++ (id)loadWithClass:(Class)loadClass owner:(id)owner;
+
++ (id)loadWithNibNamed:(NSString *)nibNamed class:(Class)loadClass owner:(id)owner;
 
 @end
